d299515b605d790d15df81c003020838093b387e
Woebenskie <42571709+Woebenskie@users.noreply.github.com>
2020-08-30 00:16:47 +0200

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='AmiiboPI',
+    version='Woebenskie',
+    packages=['joycontrol'],
+    url='https://github.com/Woebenskie/AmiiboP',
+    license='GPL-3.0',
+    author='Ruben',
+    author_email='',
+    description='To send Amiibo to Nintendo switch via a Raspberry PI with display and buttons.'
+)
